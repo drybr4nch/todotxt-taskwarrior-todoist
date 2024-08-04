b2b25d86e6cd956d0140f6c5b3e794c7991462c3
drybr4nch <tadejlenart484@gmail.com>
2024-08-04 18:46:07 +0200

    backed up tasks

--- scripts/backup/backup_tasks.sh
+++ scripts/backup/backup_tasks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 task export > "../todo/export.json"
-/usr/bin/python3 /mnt/c/Users/tadej/Documents/Projects/free/productivity/scripts/convert/taskwarrior_to_todo.py -i /mnt/c/Users/tadej/Documents/Projects/free/productivity/todo/export.json -o /mnt/c/Users/tadej/Documents/Projects/free/productivity/todo/todo_backup.txt -a ../todo/done_backup.txt
+/usr/bin/python3 /mnt/c/Users/tadej/Documents/Projects/free/productivity/scripts/convert/taskwarrior_to_todo.py -i /mnt/c/Users/tadej/Documents/Projects/free/productivity/todo/export.json -o /mnt/c/Users/tadej/Documents/Projects/free/productivity/todo/todo_backup.txt -a /mnt/c/Users/tadej/Documents/Projects/free/productivity/todo/done_backup.txt
 cp /mnt/c/Users/tadej/Documents/Projects/free/productivity/todo/todo_backup.txt /mnt/c/Users/tadej/OneDrive/Documents/todo/todo.txt
 cp /mnt/c/Users/tadej/Documents/Projects/free/productivity/todo/done_backup.txt /mnt/c/Users/tadej/OneDrive/Documents/todo/done.txt
